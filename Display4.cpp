49bb0c931c44c4eed497abc1ba66df7b1c2956d2
Nia-V <116529978+Nia-V@users.noreply.github.com>
2023-03-21 17:25:31 -0400

    Update Display4.cpp

--- Display4.cpp
+++ Display4.cpp
@@ -14,7 +14,7 @@ const byte table[10]=
   B11100110,  // 9
 };
 
-SSeg::SSeg(int D1, int D2, int D3, int D4, int latch,){
+SevSeg::SevSeg(int D1, int D2, int D3, int D4, int latch, int clock, int data){
 this->D1= D1;
 this->D2= D2;
 this->D3= D3;
@@ -23,17 +23,17 @@ this->latch = latch;
 this->clock = clock;
 this->data = data;
 }
-SSeg::init(){
+void SevSeg::init(){
   pinMode(D1, OUTPUT);
- pinMode(D2, OUTPUT);
+  pinMode(D2, OUTPUT);
   pinMode(D3, OUTPUT);
-   pinMode(D4, OUTPUT);
-   pinMode(latch, OUTPUT);
-   pinMode(clock, OUTPUT);
-   pinMode(data, OUTPUT);
+  pinMode(D4, OUTPUT);
+  pinMode(latch, OUTPUT);
+  pinMode(clock, OUTPUT);
+  pinMode(data, OUTPUT);
 }
 
-SSeg::display(int x, int y, int z, int a){
+void SevSeg::display(int x, int y, int z, int a){
 
 
     digitalWrite(D2, 1);
@@ -41,35 +41,57 @@ SSeg::display(int x, int y, int z, int a){
     digitalWrite(D4, 1);
     digitalWrite(D1, 0);
       digitalWrite(latch, 0);
-    shiftOut(data, clock, LSBFIRST, table[num3]);
+    shiftOut(data, clock, LSBFIRST, table[x]);
            digitalWrite(latch, 1);
 
 
+      digitalWrite(latch, 0);      
+digitalWrite(D1, 0);
+    digitalWrite(D2, 0);
+    digitalWrite(D3, 0);
+    digitalWrite(D4, 0);
 
 digitalWrite(D1, 1);
     digitalWrite(D3, 1);
     digitalWrite(D4, 1);
     digitalWrite(D2, 0);
       digitalWrite(latch, 0);
-    shiftOut(data, clock, LSBFIRST, table[num2]);
+    shiftOut(data, clock, LSBFIRST, table[y]);
            digitalWrite(latch, 1);
 
+
+      digitalWrite(latch, 0);      
+digitalWrite(D1, 0);
+    digitalWrite(D2, 0);
+    digitalWrite(D3, 0);
+    digitalWrite(D4, 0);
 
 digitalWrite(D1, 1);
     digitalWrite(D2, 1);
     digitalWrite(D4, 1);
     digitalWrite(D3, 0);
       digitalWrite(latch, 0);
-    shiftOut(data, clock, LSBFIRST, table[num1]);
+    shiftOut(data, clock, LSBFIRST, table[z]);
            digitalWrite(latch, 1);
-  
+
+  digitalWrite(latch, 0);      
+digitalWrite(D1, 0);
+    digitalWrite(D2, 0);
+    digitalWrite(D3, 0);
+    digitalWrite(D4, 0);
 
 digitalWrite(D1, 1);
     digitalWrite(D2, 1);
     digitalWrite(D3, 1);
     digitalWrite(D4, 0);
       digitalWrite(latch, 0);
-    shiftOut(data, clock, LSBFIRST, table[num]);
+    shiftOut(data, clock, LSBFIRST, table[a]);
            digitalWrite(latch, 1);
-}
 
+      digitalWrite(latch, 0);      
+digitalWrite(D1, 0);
+    digitalWrite(D2, 0);
+    digitalWrite(D3, 0);
+    digitalWrite(D4, 0);
+
+}
