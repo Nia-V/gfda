3ab0fba4d6a52e01e043f65674969e7d0c79c822
Nia-V <116529978+Nia-V@users.noreply.github.com>
2023-03-21 17:24:55 -0400

    Update Display4.h

--- Display4.h
+++ Display4.h
@@ -2,7 +2,8 @@
 #ifndef Display4_h//if motor class is not yet defined
 #define Display4_h// defne motr class
 #include <Arduino.h>//include arduino default library
-class SSeg {//define car class andits properties and methods
+#include "delay.h"
+class SevSeg {//define car class andits properties and methods
 private:
 //only available inside the class
 int D1;
@@ -13,8 +14,8 @@ int latch;
 int clock;
 int data;
 public:// available outside class
-SSeg();//car class overload
-SSeg(int D1, int D2, int D3, int D4, int latch, int clock, int data);
+SevSeg();//car class overload
+SevSeg(int D1, int D2, int D3, int D4, int latch, int clock, int data);
 void display(int x, int y, int z, int a);
 void init();// method for initializing car class
 
